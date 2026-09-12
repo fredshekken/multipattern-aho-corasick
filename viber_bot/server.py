785f913b30a55820c5d1b33fa6eac4c5a9807a4b
fredshekken <fmvmagaan2023@plm.edu.ph>
2026-09-12 20:58:50 +0800

    fix: updated dashboard ui

--- viber_bot/server.py
+++ viber_bot/server.py
@@ -251,7 +251,7 @@ def add_cors_headers(response):
     # server running on localhost. Not used/needed in real Viber operation.
     if SIMULATION_MODE:
         response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, DELETE, OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     return response
 
